2f2dbe8748a94f1f14a9689dac66dd5c7875e80e
olivier.dagenais <olivier.dagenais@localhost>
2010-11-14 22:49:20 -0500

    Added an AssemblyInfo.cs file to compile metadata into the assembly.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General assembly properties
+[assembly: AssemblyTitle("PivotStack")]
+[assembly: AssemblyDescription ("Converts a StackExchange dataset into a Pivot collection")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct ("PivotStack")]
+[assembly: AssemblyCopyright("(C) Copyright 2010 Olivier Dagenais")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// COM settings
+[assembly: ComVisible(false)]
+
+// Assembly GUID
+[assembly: Guid ("7a267c5a-80f4-48d3-a2e2-f518c4886e88")]
+
+// Assembly version information
+[assembly: AssemblyVersion("1.0.*")]
