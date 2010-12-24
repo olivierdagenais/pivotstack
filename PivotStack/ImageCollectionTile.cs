870b4c27c107206e2a1ccecf5d99fec1a4d89633
olivier.dagenais <olivier.dagenais@localhost>
2010-12-23 22:59:48 -0500

    Introducing the ImageCollectionTile, which describes which image(s) will end up on a given tile of a Deep Zoom Collection.

---
+++ PivotStack/ImageCollectionTile.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+using SoftwareNinjas.Core;
+
+namespace PivotStack
+{
+    public class ImageCollectionTile
+    {
+        private readonly int _row;
+        private readonly int _column;
+        private readonly string _tileName;
+        private readonly IList<Pair<int, int>> _idsToMortonNumbers;
+
+        public ImageCollectionTile (int row, int column, IEnumerable<Pair<int, int>> idsToMortonNumbers)
+        {
+            _row = row;
+            _column = column;
+            _tileName = DeepZoomImage.TileName (row, column);
+            _idsToMortonNumbers = new List<Pair<int, int>> (idsToMortonNumbers).AsReadOnly ();
+        }
+
+        public int Row
+        {
+            get
+            {
+                return _row;
+            }
+        }
+
+        public int Column
+        {
+            get
+            {
+                return _column;
+            }
+        }
+
+        public string TileName
+        {
+            get
+            {
+                return _tileName;
+            }
+        }
+
+        public IList<Pair<int, int>> IdsToMortonNumbers
+        {
+            get
+            {
+                return _idsToMortonNumbers;
+            }
+        }
+    }
+}
